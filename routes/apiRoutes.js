3ae9a25c806d9e3a32000ba56b17802ce7419e8f
Santiago MVO <santiago.mvao@gmail.com>
2020-01-07 20:24:52 -0600

    added api endpoints

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,66 @@
+// Dependencies
+// =============================================================
 var db = require("../models");
 
+// Routes
+// =============================================================
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all entries
+  app.get("/api/all", function(req, res) {
+    db.Waffle.findAll().then(function(dbWaffles) {
+      res.json(dbWaffles);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Get one entry by id
+  app.get("/api/id/:id", function(req, res) {
+    db.Waffle.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbWaffles) {
+      res.json(dbWaffles);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Get child entries of id
+  app.get("/api/children/:id", function(req, res) {
+    db.Waffle.findAll({
+      where: {
+        parentId: req.params.id
+      }
+    }).then(function(dbWaffles) {
+      res.json(dbWaffles);
     });
   });
+
+  // Add an entry
+  app.post("/api/add", function(req, res) {
+    db.Waffle.create(req.body).then(function(dbWaffle) {
+      res.json(dbWaffle);
+    });
+  });
+
+  // Hide an entry by id
+  app.put("/api/hide/:id", function(req, res) {
+    db.Waffle.update({
+      hidden:true
+    },{
+      where: { id: req.params.id }
+    }).then(function(dbWaffles) {
+      res.json(dbWaffles);
+    });
+  });
+
+  // Show an entry by id
+  app.put("/api/show/:id", function(req, res) {
+    db.Waffle.update({
+      hidden:true
+    },{
+      where: { id: req.params.id }
+    }).then(function(dbWaffles) {
+      res.json(dbWaffles);
+    });
+  });
+
 };
