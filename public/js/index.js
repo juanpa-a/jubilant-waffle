a5c16a429dd0923497a27fee87219fb519cf187a
Santiago MVO <santiago.mvao@gmail.com>
2020-01-11 11:21:48 -0600

    added parentid ref for post

--- public/js/index.js
+++ public/js/index.js
@@ -41,7 +41,7 @@ function addEntry(event) {
   event.preventDefault();
 
   var newEntry = {
-    parentId: id, //agregar aqui link a parent id
+    parentId: $(".description").attr("value"),
     //leaf: $ref.leaf.val(),
     title: $ref.title.val().trim(),
     body: $ref.body.val().trim(),
