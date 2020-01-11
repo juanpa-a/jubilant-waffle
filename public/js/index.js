1b1240187dd54cfa0f3ab611b78ca82539c7d1bd
JoaquinValle <54565586+JoaquinValle@users.noreply.github.com>
2020-01-11 11:06:22 -0600

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -45,13 +45,14 @@ function addEntry(event) {
   event.preventDefault();
 
   var newEntry = {
-    parentId: -1, //agregar aqui link a parent id
+    parentId: id, //agregar aqui link a parent id
     //leaf: $ref.leaf.val(),
     title: $ref.title.val().trim(),
     body: $ref.body.val().trim(),
     question: $ref.question.val().trim(),
     hidden: false
   };
+  console.log(id)
 
   if (!newEntry.title) {
     errorModal("Title is required.");
