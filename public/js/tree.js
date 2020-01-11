70506265e196297601d99d1059382399d96a89e7
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2020-01-11 13:15:25 -0600

    Update tree.js

--- public/js/tree.js
+++ public/js/tree.js
@@ -9,9 +9,57 @@ function wait(ms){
   }
 }
 
+let node = {
+  id: 1,
+  parentId: 0,
+  leaf: false,
+  title: "Room",
+  body:
+    "You wake up in a dark room, you look around and find no recognizable features. You feel disoriented, airless, and hungry, unsure of how long you've been in the room, or why you are here in the first place. You notice a couple of items on the floor.",
+  question: "What do you grab?",
+  hidden: false,
+  createdAt: "2020-01-10T20:55:04.000Z",
+  updatedAt: "2020-01-10T20:55:04.000Z"
+};
 
-let node = API.getById(id);
-let children = API.getByParentId(id);
+let children = [
+  {
+    id: 2,
+    parentId: 1,
+    leaf: false,
+    title: "Waffle",
+    body:
+      "You pick up the waffle, it looks relatively fresh, and the smell makes your stomach growl in anticipation. You are unsure what it's made of or if it's even safe to eat, and decide to first examine it.",
+    question: "What is it made of?",
+    hidden: false,
+    createdAt: "2020-01-10T20:55:04.000Z",
+    updatedAt: "2020-01-10T20:55:04.000Z"
+  },
+  {
+    id: 13,
+    parentId: 1,
+    leaf: false,
+    title: "Box",
+    body:
+      "You slowly notice how this ornate box glitters even in the darkness. You lose track of time as every detail entrances you, the intricate components cool to the touch. An eldritch symbol covers the keyhole, although the box appears unlocked. You slowly open the lid, revealing contents for which your sanity was unprepared.",
+    question: "What is inside?",
+    hidden: false,
+    createdAt: "2020-01-10T20:55:06.000Z",
+    updatedAt: "2020-01-10T20:55:06.000Z"
+  },
+  {
+    id: 15,
+    parentId: 1,
+    leaf: false,
+    title: "Sword",
+    body:
+      "You grab the sword's hilt and find it far lighter than you expected. You feel energy coursing through you as light surrounds the blade. You have never seen this sword before, but you know it's forever bonded to you, and that with it you are unstoppable. You hear a horrible sound outside, and turn to see a door opening that leads to a desolate valley.",
+    question: "What is outside?",
+    hidden: false,
+    createdAt: "2020-01-10T20:55:06.000Z",
+    updatedAt: "2020-01-10T20:55:06.000Z"
+  }
+];
 
 
 
